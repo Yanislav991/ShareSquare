7100d3f540b71d5efc859ece3c4fa52f44314c5d
Nikola Rashkov <nikola94rashkov@gmail.com>
2023-06-05 22:18:16 +0300

    Add register form

--- ShareSquare.Client/src/app/app.module.ts
+++ ShareSquare.Client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AppComponent } from './app.component';
     LogoComponent,
     NavComponent
   ],
+
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
--- ShareSquare.Client/src/app/pages/register/register.component.ts
+++ ShareSquare.Client/src/app/pages/register/register.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerFormData = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', Validators.email, Validators.required],
+    password: ['', Validators.required, Validators.minLength(4)]
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log(this.registerFormData.value);
+  }
 }
