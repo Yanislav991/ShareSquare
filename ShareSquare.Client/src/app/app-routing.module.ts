32b4b57fab03c8480b8147063c4ffb5d294dfee8
Nikola Rashkov <nikola94rashkov@gmail.com>
2023-06-05 21:21:20 +0300

    Modify SCSS paths and add color palette, login and register page

--- ShareSquare.Client/src/app/app-routing.module.ts
+++ ShareSquare.Client/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-
     loadComponent: () =>
       import('@pages/home/home.component').then(mod => mod.HomeComponent)
   },
@@ -13,6 +12,18 @@ const routes: Routes = [
     loadComponent: () =>
       import('@pages/profile/profile.component').then(
         mod => mod.ProfileComponent
+      )
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('@pages/login/login.component').then(mod => mod.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('@pages/register/register.component').then(
+        mod => mod.RegisterComponent
       )
   },
   {
--- ShareSquare.Client/src/app/app.module.ts
+++ ShareSquare.Client/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from '@shared/components/footer/footer.component';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { LogoComponent } from '@shared/components/logo/logo.component';
 import { NavComponent } from '@shared/components/nav/nav.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
