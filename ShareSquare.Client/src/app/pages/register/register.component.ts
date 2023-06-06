5859b1800a14c7282cf7b147cd47645031e13f08
Nikola Rashkov <nikola94rashkov@gmail.com>
2023-06-06 21:21:10 +0300

    Add latest changes

--- ShareSquare.Client/src/app/pages/register/register.component.ts
+++ ShareSquare.Client/src/app/pages/register/register.component.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
@@ -19,7 +28,7 @@ export class RegisterComponent {
 
   constructor(private fb: FormBuilder) {}
 
-  onSubmit() {
-    console.log(this.registerFormData.value);
-  }
+  // onSubmit() {
+  //   console.log(this.registerFormData.value);
+  // }
 }
