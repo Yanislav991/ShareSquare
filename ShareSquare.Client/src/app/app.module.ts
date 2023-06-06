52a91ac9d879f5ff3dd87b0794d6f44a4066b5b0
Nikola Rashkov <nikola94rashkov@gmail.com>
2023-06-06 10:55:06 +0300

    Add header and nav modifications

--- ShareSquare.Client/src/app/app.module.ts
+++ ShareSquare.Client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from '@shared/components/footer/footer.component';
 import { HeaderComponent } from '@shared/components/header/header.component';
@@ -18,7 +20,13 @@ import { AppComponent } from './app.component';
     NavComponent
   ],
 
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
